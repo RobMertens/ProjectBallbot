b201c0d602e692e54f5a355fb83335a42ad9712a
Rob Mertens <robmertens1993@gmail.com>
2017-04-28 13:11:29 +0200

    renamed:    examples/ballbot.py -> examples/balance_test.py

--- examples/balance_test.py
+++ examples/balance_test.py
@@ -4,28 +4,55 @@ Main file for the RPi-ballbot.
 @Author: Rob Mertens
 @Author: Ibe Denaux
 """
-# Import.
-# Python
-import numpy as np
-
 from src import robot
+import threading
+import time
 
-# Static vars.
+port = '/dev/ttyACM1'       # default usb port
+#port = '/dev/rfcomm0'       # default bluetooth port
 
-# Indexing:
-# R : Robot/Ballbot
-R_MARKER = 8
-R_PORT   = '/dev/ttyACM0'
+ballbot = Robot(0, port)         #make a robot/ballbot
+running = True                          #set the running flag
 
-# Objects.
-ballbot = robot(R_MARKER, R_PORT)
-ballbot.set_attitude_mode()
+# Function where the ballbot receives data
+def ballbot_update():
+    while running:
+        ballbot.receive()
+        time.sleep(0.05)
 
-"""
-Main loop.
-"""	
-while(1):
-	# Mode.
-	ballbot.set_attitude_mode()
+# Controller update
+dt = 5
+def controller_update():
+    for i in range(0,dt):
+        print (dt-i)
+        time.sleep(1)
+    ballbot.set_attitude_mode() # go to attitude mode
+    for i in range(0,dt):
+        print (dt-i)
+        time.sleep(1)
+    ballbot.set_velocity_mode() # go to velocity mode
+    while running:
+        # update the pids here
+        ballbot.set_velocity_cmd(0, 0, 0)
+        time.sleep(0.1)
+
+################
+# MAIN PROGRAM #
+################
+# Make the 2 threads and start them
+ballbot_thread = threading.Thread(None,ballbot_update,"ballbot_thread")
+controller_thread = threading.Thread(None,controller_update,"controller_thread")
+ballbot_thread.start()
+controller_thread.start()
+
+# Wait for user input to make the program halt
+raw_input("press some key...")
+# Set running false when we got a keystroke
+running = False
+# Wait for both threads to finish
+while ballbot_thread.is_alive() or controller_thread.is_alive():
+    time.sleep(0.1)
+ballbot.set_idle_mode()
+print "Stopped"
 
         
